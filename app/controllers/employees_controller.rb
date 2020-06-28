5576c5edf58e75d584abaebb30d1fda4c742bbc7
iona-b <ionabrabender@gmail.com>
2020-06-27 18:38:26 -0700

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,47 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to @employee
+        else
+            flash[:message] = @employee.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        redirect_to @employee
+    end
+
+    def destroy
+        @employee = Employee.find(params[:id])
+        @employee.delete
+        redirect_to employees_path
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,12 @@
 class Employee < ApplicationRecord
+
+    belongs_to :dog
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true
+    validates :first_name, presence: true
+
+    def to_s
+        self.first_name + " " + self.last_name
+    end
+
 end
